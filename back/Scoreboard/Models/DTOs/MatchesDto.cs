f26e03c0138ae8bbe1d24c6ae1b3dc9644a221cf
rramirezg18 <rramirezg18@miumg.edu.gt>
2025-08-24 08:14:18 -0600

    nooo

--- back/Scoreboard/Models/DTOs/MatchesDto.cs
+++ back/Scoreboard/Models/DTOs/MatchesDto.cs
@@ -1,4 +1,5 @@
-namespace Scoreboard.Api.Models.DTOs;
+//
+namespace Scoreboard.Models.DTOs;
 
 public record NewGameDto(string HomeName, string AwayName, int? QuarterDurationSeconds);
 public record NewGameByTeamsDto(int HomeTeamId, int AwayTeamId, int? QuarterDurationSeconds);
